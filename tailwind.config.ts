51d6c27c2090a40fd59d267ab3cc758cf863cbf8
Domi <107552474+DomiGames@users.noreply.github.com>
2025-01-04 15:37:04 +0100

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,11 +6,16 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: 'class', // Enable dark mode
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        vampire: {
+          dark: '#0A0A0A',
+          blood: '#8A0303',
+          gray: '#1F1F1F',
+          light: '#E5E5E5',
+        },
       },
     },
   },
