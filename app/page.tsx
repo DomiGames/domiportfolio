85f09d2832d4a9a2e59e3e03dd4b41c2255fc38f
Domi <107552474+DomiGames@users.noreply.github.com>
2025-01-04 16:03:01 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
       <div className="text-center">
         <h1 className="text-4xl font-bold text-vampire-blood mb-4">Welcome to My Portfolio</h1>
         <p className="text-lg">I build things for the web and beyond.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta quas perspiciatis rem ipsa culpa dolorum quisquam, necessitatibus facilis delectus nulla esse minima placeat error, molestias reiciendis non ipsum fuga excepturi?</p>
       </div>
     </Layout>
   );
